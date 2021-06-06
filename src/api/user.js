5f91e719cedd827c891adf352d8b14d125579fb3
dev_koo <kbi960130@naver.com>
2021-06-06 20:30:21 +0900

    프로파일 비활성화

--- src/api/user.js
+++ src/api/user.js
@@ -2,7 +2,9 @@ import http from './http';
 
 export async function getMyProfile(){
     return http.get('/api/v1/getProfile').then(response => {
-        return response.data.userNickname;
+        // return response.data.userNickname;
+        console.log(response);
+        return null;
     }).catch(error => {
         console.log(error);
         return null;
