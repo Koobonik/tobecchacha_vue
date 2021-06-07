0eda7d92e7b379547e0fab6563931374594c4748
Koobonik <kbi960130@naver.com>
2021-06-07 23:11:59 +0900

    키체인 업로드

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
+const fs = require("fs");
 module.exports = {
     devServer: {
-        disableHostCheck : true
+        disableHostCheck : true,
+        https: {
+            key: fs.readFileSync('/home/dev_koo/code/privkey.pem'),
+            cert: fs.readFileSync('/home/dev_koo/code/cert.pem'),
+            ca: fs.readFileSync('/home/dev_koo/code/chain.pem'),
+        }
     }
 }
