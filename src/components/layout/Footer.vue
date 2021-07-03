5826a87a440cb620a767a3b671a4a50425a70e5c
Koobonik <kbi960130@naver.com>
2021-07-04 01:48:58 +0900

    푸터 작업 및 책 디테일 페이지 수정

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,15 +1,38 @@
 <template>
-  <div class="footer" style="text-align: start">
-    <h4 style="color: white; font-weight: 700">문화 온도 씨도씨</h4>
-    <h6>서울특별시 종로구 청계천로 159 가동 651호  대표 : 이제경</h6>
-    <h6>사업자등록번호 : 814-06-01303  통신판매업신고번호 : 2020-서울종로-2309호</h6>
-    <h6>Copyright  ⓒ see_dosee.com. All rights Reserved</h6>
-
+  <div>
+    <div style="margin: 30px;">
+      <img style="height: 50px;" v-on:click="instagram" alt="Vue logo" src="../../assets/icons/insta_icon.jpg">
+      <img style="padding-left: 20px; height: 50px;" v-on:click="facebook" alt="Vue logo" src="../../assets/icons/facebook_icon.jpg">
+      <img style="padding-left: 20px; height: 50px;" v-on:click="kakao" alt="Vue logo" src="../../assets/icons/kakao_icon.jpg">
+    </div>
+    <b-row>
+    <div style="width: 100%; background-color: rgb(43,58,65)">
+      <div style="display: inline-block;">
+        <div class="footer" style="text-align: start;">
+          <h4 style="color: white; font-weight: 700">문화 온도 씨도씨</h4>
+          <h6>서울특별시 종로구 청계천로 159 가동 651호  대표 : 이제경</h6>
+          <h6>사업자등록번호 : 814-06-01303  통신판매업신고번호 : 2020-서울종로-2309호</h6>
+          <h6>Copyright  ⓒ see_dosee.com. All rights Reserved</h6>
+        </div>
+      </div>
+    </div>
+    </b-row>
   </div>
 </template>
 <script>
 export default {
-name: "Footer"
+name: "Footer",
+  methods: {
+    instagram(){
+      window.open("https://instagram.com", '_blank').focus();
+    },
+    facebook(){
+      window.open("https://facebook.com", '_blank').focus();
+    },
+    kakao(){
+      window.open("https://kakao.com", '_blank').focus();
+    }
+  }
 }
 </script>
 
--- src/views/books/BookDetail.vue
+++ src/views/books/BookDetail.vue
@@ -25,42 +25,79 @@
       <!-- Slides with custom text -->
     </b-carousel>
     <br>
-    <b-row>
-      <b-col style="width: 66%">
+    <b-row style="margin: 20px;">
+      <b-col style="text-align: left">
 <!--        <b-card-text>-->
 <!--          지은이 : {{books.createdWho}}-->
 <!--        </b-card-text>-->
 <!--        <b-card-text>-->
 <!--          출판 : {{books.createdWho}}-->
 <!--        </b-card-text>-->
-        <b-card-text>
-          (제목) {{books.title}}
+<!--          <div style="background-color: red; width: 300px; height: 300px; display: inline-block">-->
+<!--          </div>-->
+          <b-row style="display: inline-block;">
+              <b-row style="padding-left: 30px;">
+                  <h1 style="font-size: 22px; font-weight: bold; font-family: 'Yu Gothic'">{{books.title}}</h1>
+                  <div style="width: 20px"></div>
+                  <h6 style="font-size: 15px; font-family: 'Yu Gothic'; line-height: 22px">{{books.title}}</h6>
+              </b-row>
+          </b-row>
+
+        <b-card-text style="font-size: 13px; font-weight: bold; font-family: 'Yu Gothic'; color: rgb(116, 114, 110)">
+            {{books.createdWho}} 지음 | {{books.publishingHouse}} | {{ $moment(books.createdDate).format('YYYY년 MM월 DD일') }} 출간
         </b-card-text>
-        <b-card-text>
-          (글, 그림 외) : {{books.createdWho}}
-        </b-card-text>
-        <b-card-text>
-          (발행번호) : {{books.isbn}}
-        </b-card-text>
-        <b-card-text>
-          (페이지수) : {{books.pages}}, (제본) {{books.bookBinding}}
-        </b-card-text>
-        <b-card-text>
-          (책사이즈) : {{books.width}}cm*{{books.height}}cm*{{books.depth}}cm
-        </b-card-text>
-        <b-card-text>
-          (발행년월일) {{ $moment(books.createdDate).format('YYYY년MM월DD일') }} (발간정보) {{books.edition}} {{books.editionNumber}}쇄 {{books.price}}원
-        </b-card-text>
-        <b-card-text>
-          (제작) {{books.publishingHouse}} (가격) {{books.price}}원 <img v-on:click="openNpayLink" alt="Vue logo" src="../../assets/npay_img.png" style="height: 20px; margin-left: 10px;">
-        </b-card-text>
+          <b-row>
+              <div style="width: 70px;">
+                  <b-col>
+                      <p class="b-card-text">ISBN</p>
+                      <p class="b-card-text">쪽수</p>
+                      <p class="b-card-text">크기</p>
+                      <p class="b-card-text">가격</p>
+                  </b-col>
+              </div>
+
+              <div>
+                  <b-col>
+                      <p class="b-card-text">{{books.isbn}}</p>
+                      <p class="b-card-text">{{books.pages}}쪽</p>
+                      <p class="b-card-text">{{books.width}}mm * {{books.height}}mm * {{books.depth}}mm / </p>
+                      <p class="b-card-text">{{books.price}}원</p>
+                  </b-col>
+              </div>
+          </b-row>
+
+          <div style="font-size: 18px; font-weight: bold; margin-top: 20px; margin-bottom: 10px;">책소개</div>
+
+          <div style="font-size: 14px; font-family: 'Yu Gothic'; color: rgb(116, 114, 110);">{{books.content}}</div>
+
+          <div style=" text-align: right">
+              <img style="padding-left: 20px; height: 30px;" v-on:click="openNpayLink" alt="Vue logo" src="../../assets/npay_button.png">
+          </div>
+<!--        <b-card-text class="b-card-text">-->
+<!--          (글, 그림 외) : {{books.createdWho}}-->
+<!--        </b-card-text>-->
+<!--          <b-card-text class="b-card-text">-->
+<!--          (발행번호) : {{books.isbn}}-->
+<!--        </b-card-text>-->
+<!--          <b-card-text class="b-card-text">-->
+<!--          (페이지수) : {{books.pages}}, (제본) {{books.bookBinding}}-->
+<!--        </b-card-text>-->
+<!--          <b-card-text class="b-card-text">-->
+<!--          (책사이즈) : {{books.width}}cm*{{books.height}}cm*{{books.depth}}cm-->
+<!--        </b-card-text>-->
+<!--          <b-card-text class="b-card-text">-->
+<!--          (발행년월일) {{ $moment(books.createdDate).format('YYYY년MM월DD일') }} (발간정보) {{books.edition}} {{books.editionNumber}}쇄 {{books.price}}원-->
+<!--        </b-card-text>-->
+<!--          <b-card-text class="b-card-text">-->
+<!--          (제작) {{books.publishingHouse}} (가격) {{books.price}}원 <img v-on:click="openNpayLink" alt="Vue logo" src="../../assets/npay_img.png" style="height: 20px; margin-left: 10px;">-->
+<!--        </b-card-text>-->
 
       </b-col>
     </b-row>
-    <hr style="border: 1px solid black">
-    <b-card-text style="padding-right: 20px; padding-left: 20px; padding-top: 20px">{{books.content}}</b-card-text>
+<!--    <hr style="border: 1px solid black">-->
+<!--    <b-card-text style="padding-right: 20px; padding-left: 20px; padding-top: 20px">{{books.content}}</b-card-text>-->
 <!--    <br>-->
-    <b-card-text style="padding: 20px">{{books.tableOfContent}}</b-card-text>
+<!--    <b-card-text style="padding: 20px">{{books.tableOfContent}}</b-card-text>-->
   </div>
 
 </template>
@@ -132,5 +169,9 @@ name: "BookDetail",
 </script>
 
 <style scoped>
-
+    .b-card-text {
+        font-size: 13px;
+        line-height: 5px;
+        color: rgb(116, 114, 110);
+    }
 </style>
