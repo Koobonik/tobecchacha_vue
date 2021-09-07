b1a6ce080537724921fa37a07b08fec4e4cbdac8
Koobonik <kbi960130@naver.com>
2021-09-08 03:23:39 +0900

    동영상 베타

--- src/views/etc/EtcDetail.vue
+++ src/views/etc/EtcDetail.vue
@@ -4,7 +4,9 @@
     <b-card-text style="margin-bottom: 5px; text-align: start; margin-left: 20px;font-size: 24px; font-weight: bold; color: darkgrey"> ETC</b-card-text>
     <div class="title-css">{{etc.title}}</div>
     <div style="text-align: start; padding-left: 20px; margin-bottom: 10px;font-size: 16px; letter-spacing: -2.0px; color: grey">{{etc.subTitle}}</div>
+      <video v-if="etc.images[0].endsWith('mp4')" autoplay v-bind:src="etc.images[0]" controls loop></video>
     <b-carousel
+            v-else
         id="carousel-1"
 
         :interval=false
