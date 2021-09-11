a429460f22e37b58f16b7d4c010a55b57cc3fdd6
Koobonik <kbi960130@naver.com>
2021-09-11 14:11:06 +0900

    자간수정

--- src/components/layout/MainPageImageSlider.vue
+++ src/components/layout/MainPageImageSlider.vue
@@ -8,10 +8,12 @@
             <strong>출판<br>전시<br>교육<br>문화기획</strong>
           </div>
         <div style="align-content: end; alignment: right; flex: 2;text-align: right; padding-right: 20px">
-          <div style="text-align: start; align-content: end; alignment: right; display: inline-block; letter-spacing: -2px; line-height:25px;">
-            0도에서 100도<br>
-            액화되고 기화되는 문화의 온도<br>
-            <div style="font-weight: bold;font-size: 24px">문화온도 <strong style="font-weight: bolder;font-size: 24px">씨도씨</strong></div>
+          <div style="text-align: start; align-content: end; alignment: right; display: inline-block;">
+            <div style="line-height: 20px; letter-spacing: -3px;">
+              0도에서 100도<br>
+              액화되고 기화되는 문화의 온도<br>
+            </div>
+            <div style="font-weight: bold;font-size: 24px; letter-spacing: 0px;">문화온도 <strong style="font-weight: bolder;font-size: 24px">씨도씨</strong></div>
           </div>
         </div>
 <!--        <img   class="image2" src="../../assets/seedosee_moto.png">-->
